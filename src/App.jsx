1e4e45c8923ee35fead8f8944ad1aee1a64feeea
Bhumi Pandey <bhumipandeybp06@gmail.com>
2024-11-03 15:27:57 +0530

    new project added

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+  const [mainTask, setMainTask] = useState([])
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    setMainTask([...mainTask, {title , desc }])
+    console.log(mainTask);
+
+    setTitle("")
+    setDesc("")
+  }
+
+  const deleteHandler = (i)=>{
+    let copyTask = [...mainTask]
+    copyTask.splice(i,1)
+    setMainTask(copyTask)
+  }
+
+  let renderTask =<h2>No Task Available</h2>
+
+  if(mainTask.length>0){
+    renderTask = mainTask.map((t,i)=>{
+      return (
+      <li key={i} className='flex items-center justify-between mb-5'>
+        <div className='flex items-center justify-between w-2/3'>
+          <h5 className='text-2xl font-semibold'>{t.title}</h5>
+          <h6 className='text-lg font-medium'>{t.desc}</h6>
+        </div>
+        <button
+        onClick={()=>{
+          deleteHandler(i)
+        }}
+        className='bg-red-400 text-white px-4 py-2 rounded font-bold'>Delete</button>
+      </li>  
+      )
+    })  
+  }
+
+  return (
+    <>
+    <h1 className='bg-black text-white p-5 text-5xl text-center font '>Bhumi's Todo List</h1>
+    <form onSubmit={submitHandler}>
+      <input type='text' placeholder='Enter Title Here' className='text-2xl border-zinc-800 border-2 m-5 px-4 py-2' 
+      value={title} 
+      onChange={(e)=>{
+        setTitle(e.target.value);
+      }}
+      />
+      <input type='text' placeholder='Enter Description Here' className='text-2xl border-zinc-800 border-2 m-5 px-4 py-2' 
+      value={desc}
+      onChange={(e)=>{
+        setDesc(e.target.value)
+      }}
+      />
+      <button className='font-semibold bg-black text-white text-2xl px-3 py-2 rounded hover:bg-gray-800 m-5'>Add Task</button>
+    </form>
+    <hr/>
+    <div className='p-8 bg-slate-300'>
+      <ul>
+        {renderTask}
+      </ul>
+      </div>  
+    </>
+  )
+}
+
+export default App
